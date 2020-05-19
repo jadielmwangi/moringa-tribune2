031559f71dc908f185df6110db1ff6d49ea8eca0
jadielmwangi <jedielmwangi@gmail.com>
2020-05-19 18:26:12 +0300

    adds djago admin

---
+++ news/tests.py
@@ -0,0 +1,29 @@
+
+from django.test import TestCase
+from .models import Editor,Article,tags
+
+class EditorTestClass(TestCase):
+ # Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor))  
+
+        # Testing Save Method
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)  
+
+
+# class Editor(models.Model):
+#     first_name = models.CharField(max_length =30)
+#     last_name = models.CharField(max_length =30)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return self.first_name
+
+#     def save_editor(self):
+#         self.save()
